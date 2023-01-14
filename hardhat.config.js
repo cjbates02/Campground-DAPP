21e32123ded91692d83b375547c9bbb0cf29bbb1
Christian Bates <105766810+cjbates02@users.noreply.github.com>
2023-01-13 22:45:17 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const INFURA_API_KEY = '8aca59d070f04834a4a09b20effc0fb3'
-const GOERLI_PRIVATE_KEY = 'bbb234edf1ef093fb244ded1d7eab102e2997f1adaa683eec233c72189c74d00'
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
